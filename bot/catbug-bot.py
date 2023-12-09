10da18a8b1eeaae30888ddc73e28a0ac6cadcb71
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2023-12-09 08:58:22 +0200

    Made /leave and grouped vc commands

--- bot/catbug-bot.py
+++ bot/catbug-bot.py
@@ -1,4 +1,4 @@
-# region ===Imports===
+# region ===IMPORTS===
 
 import asyncio,discord,settings,datetime,os,csv
 from random import choice
@@ -7,15 +7,18 @@ from discord.ext import commands, tasks
 from discord import app_commands
 
 os.chdir(os.path.dirname(__file__))
-# endregion ===Imports===
+# endregion ===IMPORTS===
+# region ===DECLARATIONS===
 
 intents = discord.Intents.all() # Declare intents
 bot = commands.Bot(command_prefix="!",intents=intents) # Builds bot
 
+# endregion ===DECLARATIONS===
 # region ===ON_READY===
 
 @bot.event
 async def on_ready(): # When the client is ready
+    bot.tree.add_command(VCCommands(name="vc",description="Voice-Chat commands"))
     if bot.user:
         print(f"Signed in as {bot.user} (ID: {bot.user.id})") # Successful sign-in
     try:
@@ -48,7 +51,8 @@ async def status(interaction: discord.Interaction):
     await interaction.response.send_message(f"""Changed Status to "{quote["emoji"]}{quote["quote"]}" """,ephemeral=True)
 
 # endregion ===STATUS===
-# region ===EXAMPLE===
+# region ===COMMANDS===
+# region ==-EXAMPLE-==
 
 # @bot.tree.command(name="example",description="description") # Adds command to tree
 # @app_commands.describe(argument = "an argument",) # Adds argument description to command
@@ -59,8 +63,8 @@ async def status(interaction: discord.Interaction):
 #         voicestatus = "Not Connected"
 #     await interaction.response.send_message(f"This channel is:`{interaction.channel}`\nYou are:`{interaction.user}`\nThe message of this interaction is:`{interaction.message}`\nThe data of this interaction is:`{interaction.data}`\nYour VC status is:`{voicestatus}`\nThe argument you entered was:`{argument}`")
 
-# endregion ===EXAMPLE===
-# region ===Silly===
+# endregion ==-EXAMPLE-==
+# region ==-SILLY-==
 
 @bot.tree.command(name="penis",description="( ͡° ͜ʖ ͡°)")
 @app_commands.describe(visible="Make output visible in channel?")
@@ -73,8 +77,8 @@ async def example(interaction: discord.Interaction, member: Optional[discord.Mem
     length = (int(member.id) % 11) + 1
     await interaction.response.send_message(f"{member.mention}:\n8"+"=".center(length,"=")+"D",ephemeral=not visible)
 
-# endregion ===Silly===
-# region ===CLI===
+# endregion ==-SILLY-==
+# region ==-CLI-==
 
 @bot.tree.command(name="pwd",description="tells you *where* you are") # Also syncs commands
 @app_commands.describe(visible="Make output visible in channel?")
@@ -135,28 +139,51 @@ async def whois(interaction: discord.Interaction, member: Optional[discord.Membe
             message += f"[Guild Avatar]({member.guild_avatar.url})\n"
     await interaction.response.send_message(content=message,ephemeral=not visible)
 
-# endregion ===CLI===
-# region ===VC===
+# endregion ==-CLI-==
+# region ==-VC-==
 
-@bot.tree.command(name="join",description="asks the bot to join your VC")
-async def join(interaction: discord.Interaction):
-    if isinstance(interaction.user,discord.Member):
-        if not interaction.user.voice:
-            await interaction.response.send_message("You must be connected to a VC to use this command",ephemeral=True)
+class VCCommands(app_commands.Group): # Create Group
+    @app_commands.command(name="join",description="Join Voice-Chat")
+    async def join(self,interaction: discord.Interaction): # Don't forget `self`
+        if not interaction.guild: # If DM, send "nuh-uh"
+            await interaction.response.send_message("This command can only be used in servers, sorry!",ephemeral=True)
             return
-        elif interaction.user.voice.channel:
-            try:
-                await interaction.user.voice.channel.connect()
-                await interaction.response.send_message(f"Connected to {interaction.user.voice.channel}",ephemeral=True) 
-            except:
-                await interaction.response.send_message(f"Could not connect to {interaction.user.voice.channel}",ephemeral=True)
-    else:
-        await interaction.response.send_message(f"Error encountered: You are not recognised as a member. Either you are not in a guild or there is something else going on.\nFor reference, your user is {interaction.user}",ephemeral=True)
+        elif isinstance(interaction.user,discord.Member): # Check that the right info is there
+            if not interaction.user.voice: # Check that user is in the channel
+                await interaction.response.send_message("You must be connected to a VC to use this command",ephemeral=True) # If they aren't, send "nuh-uh"
+                return
+            elif interaction.user.voice.channel: # Check that we know what channel the user is in
+                try: # Try connecting
+                    await interaction.user.voice.channel.connect()
+                    await interaction.response.send_message(f"Connected to {interaction.user.voice.channel}",ephemeral=True) # Nailed it
+                except: # If we fail to connect say why
+                    await interaction.response.send_message(f"Could not connect to {interaction.user.voice.channel}",ephemeral=True) # Damn it
+        else: # Something has gone wrong, so say that and give some info.
+            await interaction.response.send_message(f"Error encountered: You are not recognised as a member.\nUser:`{interaction.user}`\nGuild:`{interaction.guild}`",ephemeral=True)
 
-@bot.tree.command(name="atc",description="streams radio traffic")
+    @app_commands.command(name="leave",description="Leave Voice-Chat")
+    async def leave(self,interaction: discord.Interaction): # Don't forget `self`
+        if not interaction.guild: # If DM, send "nuh-uh"
+            await interaction.response.send_message("This command can only be used in servers, sorry!",ephemeral=True)
+            return
+        elif isinstance(interaction.guild,discord.Guild): # Check that guild info is there
+            if not interaction.guild.voice_client: # Check that bot is connected in that server.
+                await interaction.response.send_message("I am not connected to a VC in this server.",ephemeral=True) # If it's not, send "nuh-uh"
+                return
+            else:
+                try: # Try disconnecting
+                    await interaction.guild.voice_client.disconnect(force=True)
+                    await interaction.response.send_message(f"Disconnected.",ephemeral=True) # Nailed it
+                except: # If we fail to disconnect say it
+                    await interaction.response.send_message(f"Could not disconnect!?\nHelp!?!?",ephemeral=True) # Damn it
+        else: # Something has gone wrong, so say that and give some info.
+            await interaction.response.send_message(f"Error encountered: Guild not recognised.\nGuild:`{interaction.guild}`",ephemeral=True)
+
+@bot.tree.command(name="atc",description="WIP - Streams Radio Traffic")
 @app_commands.describe(airport = "Enter an Airport code")
-async def atc(interaction: discord.Interaction, airport: str):
+async def atc(interaction: discord.Interaction, airport: Optional[str] = "blank"):
     await interaction.response.send_message(f"This feature is still under construction, but for now:\nYou requested the {airport} airport.",ephemeral=True)
-# endregion ===VC===
+# endregion ==-VC-==
+# endregion ===COMMANDS===
 
 bot.run(settings.TOKEN)
