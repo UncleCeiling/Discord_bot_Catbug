2f20c7990522ac479d96189bc9382c33f4931961
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2023-12-09 04:26:00 +0200

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Syncing Repo"
+git fetch
 echo "Starting catbug-bot.py"
-sudo nohup python3 ~/Catbug-bot/catbug-bot.py > output.log
+sudo nohup python3 ~/Catbug/bot/catbug-bot.py > output.log
