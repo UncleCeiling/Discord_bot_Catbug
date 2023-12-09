5858f21d0b16e37401cf4cc2d87680099f72f9a2
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2023-12-09 03:30:40 +0200

    Completed Cycling Status with quotes

--- README.md
+++ README.md
@@ -5,24 +5,25 @@ A stupid bot amongst friends.
 Starting out as a project to make a bot that can stream Air-Traffic Control radio chatter into a Discord channel, it has spiralled into a ressurection of the long-dead ***Catbug*** bot.
 ___
 ## Feature List:
+- Visibility toggle for most commands
 - CLI commands
-  - pwd
-  - whoami
-  - whois [member]
-- penis [member] (hehehe)
-___
-## To-do List
-- ATC commands
-  - atc
-  - atc [airport/search]
+  - /pwd
+  - /whoami
+  - /whois [member]
+- /penis [member] (hehehe)
 - Catbug References
   - Statuses
     - Cycling Quotes from a list.
-    - Status refresh command 
+    - /status (randomises a new status) 
+___
+## To-do List
+- Equipment Generator (From an old Project)
+- ATC commands
+  - atc [airport/search]
+- Catbug References
   - Commands
     - gifs
     - Sugarpeas!
     - sounds
-- Equipment Generator (From an old Project)
 - Flirt/Rizz
 - A request from CCFLamez for an auto-mute/unmute based on rich presence features.
--- bot/catbug-bot.py
+++ bot/catbug-bot.py
@@ -1,13 +1,12 @@
 # region ===Imports===
 
-import asyncio,discord,os,settings,youtube_dl,datetime
-from typing import Optional, Type
-from venv import logger
-from queue import Empty
-from email.mime import application
+import asyncio,discord,settings,datetime,os
+from random import choice
+from typing import Optional
 from discord.ext import commands, tasks
-from discord import Interaction, Member, User, app_commands, FFmpegPCMAudio
+from discord import app_commands
 
+os.chdir(os.path.dirname(__file__))
 # endregion ===Imports===
 
 intents = discord.Intents.all() # Declare intents
@@ -24,8 +23,29 @@ async def on_ready(): # When the client is ready
         print(f"Synced {len(synced)} command(s)") # Display results
     except Exception as exception:
         print(exception)
+    status_task.start()
 
 # endregion ===ON_READY===
+# region ===STATUS===
+
+def new_quote():
+    with open("files/quotes.txt") as file:
+        quotes = file.readlines()
+    return choice(quotes).strip()
+
+@tasks.loop()
+async def status_task() -> None:
+    quote = new_quote()
+    await bot.change_presence(activity=discord.CustomActivity(name=quote))
+    await asyncio.sleep(300)
+
+@bot.tree.command(name="status",description="picks a new random status")
+async def status(interaction: discord.Interaction):
+    quote = new_quote()
+    await bot.change_presence(activity=discord.CustomActivity(name=quote))
+    await interaction.response.send_message(f"""Changed Status to "{quote}" """,ephemeral=True)
+
+# endregion ===STATUS===
 # region ===EXAMPLE===
 
 # @bot.tree.command(name="example",description="description") # Adds command to tree
@@ -38,33 +58,40 @@ async def on_ready(): # When the client is ready
 #     await interaction.response.send_message(f"This channel is:`{interaction.channel}`\nYou are:`{interaction.user}`\nThe message of this interaction is:`{interaction.message}`\nThe data of this interaction is:`{interaction.data}`\nYour VC status is:`{voicestatus}`\nThe argument you entered was:`{argument}`")
 
 # endregion ===EXAMPLE===
-# region ===CATBUG===
+# region ===Silly===
 
 @bot.tree.command(name="penis",description="( ͡° ͜ʖ ͡°)")
-@app_commands.describe(member = ":eyes:")
-async def example(interaction: discord.Interaction, member: Optional[discord.Member]):
+@app_commands.describe(visible="Make output visible in channel?")
+async def example(interaction: discord.Interaction, member: Optional[discord.Member], visible: bool = False):
     if isinstance(interaction.user,discord.Member):
         member = member or interaction.user
     else:
-        await interaction.response.send_message("Error - member does not exist")
+        await interaction.response.send_message("Error - member does not exist",ephemeral=not visible)
         return
     length = (int(member.id) % 11) + 1
-    await interaction.response.send_message(f"{member.mention}:\n8"+"=".center(length,"=")+"D")
+    await interaction.response.send_message(f"{member.mention}:\n8"+"=".center(length,"=")+"D",ephemeral=not visible)
 
-# endregion ===CATBUG===
+# endregion ===Silly===
 # region ===CLI===
 
 @bot.tree.command(name="whoami",description="tells you *who* you are")
-async def whoami(interaction: discord.Interaction):
-    await interaction.response.send_message(f"`{interaction.user}`")
+@app_commands.describe(visible="Make output visible in channel?")
+async def whoami(interaction: discord.Interaction,visible: bool = False):
+    await interaction.response.send_message(f"`{interaction.user}`",ephemeral=not visible)
 
 @bot.tree.command(name="pwd",description="tells you *where* you are")
-async def pwd(interaction: discord.Interaction):
-    await interaction.response.send_message(f"`{interaction.guild}/{interaction.channel}`")
+@app_commands.describe(visible="Make output visible in channel?")
+async def pwd(interaction: discord.Interaction,visible: bool = False):
+    try:
+        synced = await bot.tree.sync() # Sync Command Tree
+        print(f"Synced {len(synced)} command(s)") # Display results
+    except Exception as exception:
+        print(exception)
+    await interaction.response.send_message(f"`{interaction.guild}/{interaction.channel}`",ephemeral=not visible)
 
-@bot.tree.command(name="whois",description="tells you who *someone else* is")
-@app_commands.describe(member = "The person you want to know more about (leave empty for yourself)")
-async def whois(interaction: discord.Interaction, member: Optional[discord.Member]):
+@bot.tree.command(name="whois",description="Prints out a bunch of information")
+@app_commands.describe(member = "The person you want to know more about (leave empty for yourself)",visible="Make output visible in channel?")
+async def whois(interaction: discord.Interaction, member: Optional[discord.Member], visible: bool = False):
     message = ""
     if isinstance(interaction.user,discord.Member):
         member = member or interaction.user
@@ -105,8 +132,7 @@ async def whois(interaction: discord.Interaction, member: Optional[discord.Membe
             message += f"[Main Avatar]({member.display_avatar.url})\n"
         if member.guild_avatar:
             message += f"[Guild Avatar]({member.guild_avatar.url})\n"
-
-    await interaction.response.send_message(content=message)
+    await interaction.response.send_message(content=message,ephemeral=not visible)
 
 # endregion ===CLI===
 # region ===VC===
@@ -115,21 +141,21 @@ async def whois(interaction: discord.Interaction, member: Optional[discord.Membe
 async def join(interaction: discord.Interaction):
     if isinstance(interaction.user,discord.Member):
         if not interaction.user.voice:
-            await interaction.response.send_message("You must be connected to a VC to use this command")
+            await interaction.response.send_message("You must be connected to a VC to use this command",ephemeral=True)
             return
         elif interaction.user.voice.channel:
             try:
                 await interaction.user.voice.channel.connect()
-                await interaction.response.send_message(f"Connected to {interaction.user.voice.channel}") 
+                await interaction.response.send_message(f"Connected to {interaction.user.voice.channel}",ephemeral=True) 
             except:
-                await interaction.response.send_message(f"Could not connect to {interaction.user.voice.channel}")
+                await interaction.response.send_message(f"Could not connect to {interaction.user.voice.channel}",ephemeral=True)
     else:
-        await interaction.response.send_message(f"Error encountered: You are not recognised as a member. Either you are not in a guild or there is something else going on.\nFor reference, your user is {interaction.user}")
+        await interaction.response.send_message(f"Error encountered: You are not recognised as a member. Either you are not in a guild or there is something else going on.\nFor reference, your user is {interaction.user}",ephemeral=True)
 
 @bot.tree.command(name="atc",description="streams radio traffic")
 @app_commands.describe(airport = "Enter an Airport code")
 async def atc(interaction: discord.Interaction, airport: str):
-    await interaction.response.send_message(f"This feature is still under construction, but for now:\nYou requested the {airport} airport.")
+    await interaction.response.send_message(f"This feature is still under construction, but for now:\nYou requested the {airport} airport.",ephemeral=True)
 # endregion ===VC===
 
 bot.run(settings.TOKEN)
