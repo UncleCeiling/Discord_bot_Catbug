04dbf11e5a81a9b6a7ad4215ee9bf6666cd10f8d
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2023-12-17 22:15:31 +0200

    Trying new cron_start

--- cron_start.sh
+++ cron_start.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-cd /home/minipi/Catbug
-git fetch
-git pull --rebase
-python3 /home/minipi/Catbug/bot/catbug-bot.py
+cd /home/minipi/Catbug && git fetch && git pull --rebase && python3 /home/minipi/Catbug/bot/catbug-bot.py
