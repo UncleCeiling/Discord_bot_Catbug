cb6605d50f0b0e9e19514cb4d0984c7ba93c8410
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2023-12-17 22:49:46 +0200

    Added reset

--- cron_start.sh
+++ cron_start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cd /home/minipi/Catbug ; git fetch ; git pull --rebase ; python3 /home/minipi/Catbug/bot/catbug-bot.py
+cd /home/minipi/Catbug
+git fetch --all
+git reset --hard
+git stash pop
+git pull
+python3 /home/minipi/Catbug/bot/catbug-bot.py
