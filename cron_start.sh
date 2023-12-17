3773d292a372c21bb46cfd6db707cb185c31251d
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2023-12-17 23:13:58 +0200

    Simplified

--- cron_start.sh
+++ cron_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/minipi/Catbug
-git fetch --all && git reset --hard && git stash pop && git pull && python3 /home/minipi/Catbug/bot/catbug-bot.py
+git pull && python3 /home/minipi/Catbug/bot/catbug-bot.py
