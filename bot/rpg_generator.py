d1e3856080ae587db60da18e3a68619b1d51999c
Chris Farfan <138206825+ChrisFarfan-CN@users.noreply.github.com>
2023-12-18 12:58:03 +0200

    Moved and renamed wordlists

---
+++ bot/rpg_generator.py
@@ -0,0 +1,35 @@
+from os import chdir, path
+from random import sample
+
+def import_rpg_words(path_to_lists: str):
+    """Returns a Dict containing 4 lists, taken from the files in the specified path:
+
+    {"adjectives" = [adjectives.csv],
+    "weapons" = [weapons.csv],
+    "armour" = [armour.csv],
+    "nouns" = [nouns.csv]}
+
+    On ERROR returns None"""
+    try:
+        adjectives = (open(path_to_lists + "adjectives.csv","r").readlines())[0].split(",")
+        weapons = (open(path_to_lists + "weapons.csv", "r").readlines())[0].split(",")
+        armour = (open(path_to_lists + "armour.csv", "r").readlines())[0].split(",")
+        nouns = (open(path_to_lists + "nouns.csv", "r").readlines())[0].split(",")
+        rpg_words = {"adjectives":adjectives,"weapons":weapons,"armour":armour,"nouns":nouns}
+        return rpg_words
+    except:
+        return
+
+def gen_equipment(equipment_list:list[str],noun_list:list[str]):
+    """Returns a random combination of equipment and noun from the given lists."""
+    sample1 = sample(equipment_list,1)[0]
+    sample2 = sample(noun_list,1)[0]
+    return (f"{sample1} of {sample2}".title())
+
+def add_buff(equipment_in:str,num_of_buffs:int,adjectives_list:list[str]):
+    """Returns a buffed version of a given bit of equipment.
+    
+    Takes the equipment to be buffed, number of buffs and list of adjectives."""
+    sampled_adjectives = sample(adjectives_list,num_of_buffs)
+    buff = " ".join(sampled_adjectives)
+    return (f"{buff} {equipment_in}".title())
