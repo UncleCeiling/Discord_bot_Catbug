c448b567f511ef5492759c6a2368d95976ac8688
Chris Farfan de los Godos <chrisfarfandelosgodos@gmail.com>
2024-01-08 01:09:59 +0200

    Fixed reboot log output

--- cogbot/cogs/system_cog.py
+++ cogbot/cogs/system_cog.py
@@ -90,7 +90,7 @@ class System(commands.Cog):
                 await interaction.response.send_message(message,ephemeral=False)
                 message += "\n\n> 🪝 Running `git pull`..."
                 await interaction.edit_original_response(content=message)
-                output = subprocess.run(["git","pull"],stdout=subprocess.PIPE).stdout.decode("utf-8").replace("Fast-forward","FastForward").replace("+","🟢").replace("-","🔴").replace("Already up to date.","Already up to date. ✅")
+                output = subprocess.run(["git","pull"],stdout=subprocess.PIPE).stdout.decode("utf-8").replace("Fast-forward","FastForward").replace("+","🟢").replace("-","🔴").replace("Already up to date.","Already up to date. ✅").replace("\n","\n> ")
                 message += f"\n\n> 🗒️ Output:\n> {output}"
                 await interaction.edit_original_response(content=message)
                 message += "\n\n> 🚪 Logging off..."
