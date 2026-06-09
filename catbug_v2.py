497b0aeb9608698fccd9ba16e1b5112127ac5904
ChrisOnTheCeiling <chris@ontheceiling.eu>
2026-06-09 21:08:46 +0300

    Fix for broken commands

--- catbug_v2.py
+++ catbug_v2.py
@@ -30,7 +30,6 @@ async def on_ready():
     print("> Syncing")
     for guild in guilds:
         try:
-            bot.tree.copy_global_to(guild=guild)
             synced_list = await bot.tree.sync(guild=guild)
         except Exception as exception:
             print(f"> ERROR: \n\n```{exception}```")
