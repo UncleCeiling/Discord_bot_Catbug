f82c0dfa2b5c6dd4fde0b1d53478ea4f62de3367
ChrisOnTheCeiling <chris@ontheceiling.eu>
2026-06-10 12:53:42 +0300

    debugging

--- cogs/dm_cog.py
+++ cogs/dm_cog.py
@@ -11,15 +11,14 @@ class DM(commands.Cog):
         print("> dm_cog loaded")
     
     @app_commands.command(name="sync",description="Syncs the bot commands")
-    @app_commands.allowed_contexts(False,True,False)
     @app_commands.dm_only()
     @app_commands.describe(command="command to run")
     @app_commands.choices(command=[
         app_commands.Choice(name="global", value="1"),
         app_commands.Choice(name="guilds", value="2"),
         app_commands.Choice(name="clear global", value="3"),
-        app_commands.Choice(name="clear guilds", value="4"),
-        app_commands.Choice(name="global to guilds", value="5"),
+        app_commands.Choice(name="clear guild", value="4"),
+        app_commands.Choice(name="copy global to guilds", value="5"),
         ])
     async def example(self, interaction: discord.Interaction, command: app_commands.Choice[str]):
         with open("data/admins.csv",encoding="utf8") as file:
@@ -61,24 +60,26 @@ class DM(commands.Cog):
             case 4:
                 guilds = self.bot.guilds
                 cleared_servers = 0
-                message = "> Clearing"
-                await interaction.response.send_message(message,ephemeral=False)
+                # message = "> Clearing"
+                # await interaction.response.send_message(message,ephemeral=False)
+                message = f"```{self.bot.commands}```"
                 for guild in guilds:
-                    try:
-                        self.bot.tree.clear_commands(guild=guild)
-                        self.bot.tree.copy_global_to(guild=guild)
-                        synced_list = await self.bot.tree.sync(guild=guild)
-                    except Exception as exception:
-                        message += f"\n> ERROR: \n```{exception}```"
-                        await interaction.edit_original_response(content=message)
-                    else:
-                        message += f"\n> > Syncing cleared {len(synced_list)} commands to `{guild.name}`"
-                        for synced_command in synced_list:
-                            message += f"\n> > > {synced_command.name}"
-                        cleared_servers += 1
-                        await interaction.edit_original_response(content=message)
-                message += f"\n> Cleared {cleared_servers} servers."
-                await interaction.edit_original_response(content=message)
+                    message = f"\n```{guild}```"
+                await interaction.response.send_message(message)
+                #         self.bot.tree.clear_commands(guild=guild)
+                #         self.bot.tree.copy_global_to(guild=guild)
+                #         synced_list = await self.bot.tree.sync(guild=guild)
+                #     except Exception as exception:
+                #         message += f"\n> ERROR: \n```{exception}```"
+                #         await interaction.edit_original_response(content=message)
+                #     else:
+                #         message += f"\n> > Syncing cleared {len(synced_list)} commands to `{guild.name}`"
+                #         for synced_command in synced_list:
+                #             message += f"\n> > > {synced_command.name}"
+                #         cleared_servers += 1
+                #         await interaction.edit_original_response(content=message)
+                # message += f"\n> Cleared {cleared_servers} servers."
+                # await interaction.edit_original_response(content=message)
             case 5:
                 guilds = self.bot.guilds
                 synced_servers = 0
