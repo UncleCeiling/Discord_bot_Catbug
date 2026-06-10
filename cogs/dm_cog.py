9051c890320b32202f213a63f94467eaded49d79
ChrisOnTheCeiling <chris@ontheceiling.eu>
2026-06-10 11:59:19 +0300

    unused description

--- cogs/dm_cog.py
+++ cogs/dm_cog.py
@@ -12,7 +12,7 @@ class DM(commands.Cog):
     
     @app_commands.command(name="sync",description="Syncs the bot commands")
     @app_commands.allowed_contexts(False,True,False)
-    @app_commands.describe(command="command",visible="Make output visible in channel?")
+    @app_commands.describe(command="command")
     @app_commands.choices(command=[
         app_commands.Choice(name="global", value="1"),
         app_commands.Choice(name="guilds", value="2"),
