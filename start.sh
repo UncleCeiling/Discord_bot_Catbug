2e35820e4041f3265b7141865bada654483881f7
ChrisOnTheCeiling <chris@ontheceiling.eu>
2026-06-13 16:35:59 +0300

    Need to actually assign the variable?

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 echo "> Updating from Github";
 git pull;
 echo "> Composing container";
-docker compose up --build -d
-docker wait catbug
-EXIT_CODE = $?;
-echo $EXIT_CODE;
+docker compose up --build -d;
+EXIT_CODE = $(docker wait catbug);
+echo "Exited with $EXIT_CODE";
