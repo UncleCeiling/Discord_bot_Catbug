9afc2d4a2fbec06ce75744ea7082f9ce93d5f08b
ChrisOnTheCeiling <chris@ontheceiling.eu>
2026-06-13 16:50:00 +0300

    Made bash script a loop

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-echo "> Updating from Github"
-git pull
-echo "> Composing container"
-docker compose up --build -d
-code=$(docker wait catbug)
-echo "Exited with $code"
+while ; do
+    # Update
+    echo ">> Updating from Github."
+    git pull
+    # Start container and wait for exit code
+    echo ">> Composing container."
+    code=$(docker compose up --build)
+    # docker compose up --build -d
+    # code=$(docker wait catbug)
+    echo ">> Catbug exited with $code."
+    if [$code -ne 0]; then
+        echo ">> Stopping."
+        exit 1
+    fi
+    echo ">> Restarting."
+done
