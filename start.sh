c40536dedbf01216c619a5e3e3aadbfe66656d36
ChrisOnTheCeiling <chris@ontheceiling.eu>
2026-06-13 16:38:11 +0300

    did the space mess with it?

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ echo "> Updating from Github";
 git pull;
 echo "> Composing container";
 docker compose up --build -d;
-EXIT_CODE = $(docker wait catbug);
+EXIT_CODE=$(docker wait catbug);
 echo "Exited with $EXIT_CODE";
